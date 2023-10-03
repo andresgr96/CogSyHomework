402b290e3719f19ec6257fbfd37dcb338cac400b
andres <andresgarciar96@gmail.com>
2023-10-03 16:29:59 +0200

    finished assignment 2

--- Ass2/Ass2R.R
+++ Ass2/Ass2R.R
@@ -1,35 +1,50 @@
-library(lme4) # This is the package that we need for using LMMs
+# Imports
+library(lme4) 
 
 
-# First, specify the path to the datafile:
-setwd('C://Users//Josh//Desktop//Josh work//Courses//Gastcolleges//LMM workshop')
+# Set Path and Load File
+setwd('C:\\Users\\andre\\Desktop\\CogSyHomework\\Ass2')
 data <- read.csv('data.txt')
 
 
-# For the analysis of RTs, we want to exclude incorrectly answered trials:
+# Clean data
 data <- data[data$error == 0,]
 data <- data[data$final_stimulus == 'target',]
 
 
-# And we want to remove outliers:
+# Remove outliers:
 outlier1 <- mean(data$RT) + 2.5 * sd(data$RT)
 outlier2 <- mean(data$RT) - 2.5 * sd(data$RT)
 data <- data[data$RT > outlier2,]
 data <- data[data$RT < outlier1,]
 
+# Declare Factors
+data$target_background <- as.factor(data$target_background)
 
-# Here is the code for running a very simple LMM with distractor as fixed effect,
-# and by-subject and by-item intercepts as random effect
 
+#Intercepts, Slopes and Models
 model1 <- lmer(
-    RT ~ distractor + (1|subject)+(1|item),
-    data=data, )
+  pupil_size ~ target_background + (1|subject)+(1|RT)+(1|target_side),
+  data=data,)
+
+model2 <- lmer(
+  pupil_size ~ target_background * target_side + (1+target_side|subject)+(1|RT),
+  data=data,)
+
+model3 <- lmer(
+  pupil_size ~ target_background * RT + (1+target_side|subject)+(1|target_side),
+  data=data,)
+
+model4 <- lmer(
+  pupil_size ~ target_background + (1+ target_side|subject)+(1|RT)+(1|target_side),
+  data=data,)
 
 
-
-
-# To read out the results, request a summary of the model
+# Showcase results
 summary(model1)
+summary(model2)
+summary(model3)
+summary(model4)
 
 
 
